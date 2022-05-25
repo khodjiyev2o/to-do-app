97eeead0da78f42d7fc7cdd1ce427446f0da7343
khodjiyev2o <92217667+khodjiyev2o@users.noreply.github.com>
2022-05-25 11:02:19 +0500

    changes

--- drf/backend/api/serializers.py
+++ drf/backend/api/serializers.py
@@ -3,22 +3,6 @@ from rest_framework import serializers
 
 
 class ProductSerializer(serializers.ModelSerializer):
-
-
     class Meta:
         model = Product
-        fields = ['id','title','completed']
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        fields = ['id', 'title', 'completed']
