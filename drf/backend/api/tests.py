d4a8613341bd87b9ead401eb3b3331aec513bcc3
khodjiyev2o <92217667+khodjiyev2o@users.noreply.github.com>
2022-06-22 19:40:48 +0900

    unittest

--- drf/backend/api/tests.py
+++ drf/backend/api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from .models import Product
+from django.contrib.auth.models import User
+from django.test import Client
 # Create your tests here.
+
+
+class TodoTest(TestCase):
+    def setup(self):
+        obj1 = Product.objects.create(title='clean the house',completed=False)
+
+    def test_request(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code,200)
+
+
+    def test_request_api(self):
+        client = Client()
+        response = client.get('/api/task-list/')
+        self.assertEqual(response.status_code, 200)
+    
