d9adfd60c4b31ea55ccb6e91597ca51c11684377
khodjiyev2o <92217667+khodjiyev2o@users.noreply.github.com>
2022-06-23 12:42:11 +0900

    selenium testing

--- drf/backend/api/tests.py
+++ drf/backend/api/tests.py
@@ -3,20 +3,40 @@ from .models import Product
 from django.contrib.auth.models import User
 from django.test import Client
 # Create your tests here.
+import os
+import pathlib
+import unittest
+from selenium import webdriver
+
+os.environ['PATH'] += r'C:\Users\django\selenium'
+
+
+def file_uri(filename):
+    return pathlib.Path(os.path.abspath(filename)).as_uri()
+
+
+driver = webdriver.Chrome()
+
+
+class Webpagetests(unittest.TestCase):
+
+    def test_title(self):
+        uri = file_uri('api/templates/sel.html')
+        driver.get(uri)
+        self.assertEqual(driver.title, 'TO DO')
+
 
 
 class TodoTest(TestCase):
     def setup(self):
-        obj1 = Product.objects.create(title='clean the house',completed=False)
+        obj1 = Product.objects.create(title='clean the house', completed=False)
 
     def test_request(self):
         client = Client()
         response = client.get('/')
-        self.assertEqual(response.status_code,200)
-
+        self.assertEqual(response.status_code, 200)
 
     def test_request_api(self):
         client = Client()
         response = client.get('/api/task-list/')
         self.assertEqual(response.status_code, 200)
-    
