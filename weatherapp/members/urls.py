5690a201aacbdcecdbccff6cd85db535b248d12f
Samandar <samandarkhodjiyev@gmail.com>
2022-07-20 21:35:09 +0900

    deleted unnecessary files

--- weatherapp/members/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('',views.index,name='login'),
-    path('logout',views.logout_user,name='logout'),
-    path('register', views.register_user, name='register'),
-
-    ]
